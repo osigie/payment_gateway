0a8d3a47f9b6a998b149f22c6b3a1b5ebeeed9df
osigie <kenosagie88@gmail.com>
2026-07-09 05:27:47 +0100

    feat: added entity

--- src/main/java/com/osigie/payment_gateway/domain/PaymentStatus.java
+++ src/main/java/com/osigie/payment_gateway/domain/PaymentStatus.java
@@ -1,4 +1,5 @@
 package com.osigie.payment_gateway.domain;
 
 public enum PaymentStatus {
+    PENDING, AUTHORIZED, CAPTURED, FAILED, REFUNDED, VOIDED
 }
--- src/main/java/com/osigie/payment_gateway/domain/TransactionType.java
+++ src/main/java/com/osigie/payment_gateway/domain/TransactionType.java
@@ -1,4 +1,5 @@
 package com.osigie.payment_gateway.domain;
 
 public enum TransactionType {
+    AUTHORIZED, CAPTURE, REFUND, VOID
 }
