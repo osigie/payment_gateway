c3c3400c140bd3382fa7adb999759f327c8c674d
osigie <kenosagie88@gmail.com>
2026-07-11 12:02:30 +0100

    feat: wire up the dto and result class changes

--- src/main/java/com/osigie/payment_gateway/controller/PaymentController.java
+++ src/main/java/com/osigie/payment_gateway/controller/PaymentController.java
@@ -1,5 +1,12 @@
 package com.osigie.payment_gateway.controller;
 
+import com.osigie.payment_gateway.domain.entity.Payment;
+import com.osigie.payment_gateway.dto.BaseResponse;
+import com.osigie.payment_gateway.dto.ResponseMapper;
+import com.osigie.payment_gateway.dto.Result;
+import com.osigie.payment_gateway.dto.payment.CreateAuthorizationRequestDto;
+import com.osigie.payment_gateway.dto.payment.PaymentResponse;
+import com.osigie.payment_gateway.mapper.PaymentMapper;
 import com.osigie.payment_gateway.service.PaymentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +21,18 @@ public class PaymentController {
 
 
     private final PaymentService paymentService;
+    private final PaymentMapper paymentMapper;
 
-    public PaymentController(PaymentService paymentService) {
+    public PaymentController(PaymentService paymentService, PaymentMapper paymentMapper) {
         this.paymentService = paymentService;
+        this.paymentMapper = paymentMapper;
     }
 
     @PostMapping
-    public ResponseEntity<?> createPayment(@RequestBody Object payment) {
-        return ResponseEntity.ok(null);
+    public ResponseEntity<BaseResponse<PaymentResponse>> createAuthorize(@RequestBody CreateAuthorizationRequestDto dto) {
+        Result<Payment> payment = paymentService.createAuthorize(dto);
+        Result<PaymentResponse> paymentDto = payment.map(paymentMapper::toDto);
+        return ResponseMapper.toResponse(paymentDto);
     }
 
 
--- src/main/java/com/osigie/payment_gateway/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/osigie/payment_gateway/service/impl/PaymentServiceImpl.java
@@ -1,8 +1,16 @@
 package com.osigie.payment_gateway.service.impl;
 
+import com.osigie.payment_gateway.domain.entity.Payment;
+import com.osigie.payment_gateway.dto.Result;
+import com.osigie.payment_gateway.dto.payment.CreateAuthorizationRequestDto;
 import com.osigie.payment_gateway.service.PaymentService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public Result<Payment> createAuthorize(CreateAuthorizationRequestDto dto) {
+        return null;
+    }
 }
