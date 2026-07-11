83c3296b4bc50333588c8a74564216438a2b26da
osigie <kenosagie88@gmail.com>
2026-07-11 20:41:23 +0100

    feat: created AtomicPhaseExecutor for all DAG implementation

---
+++ src/main/java/com/osigie/payment_gateway/service/impl/AtomicPhaseExecutorImpl.java
@@ -0,0 +1,74 @@
+package com.osigie.payment_gateway.service.impl;
+
+import com.osigie.payment_gateway.domain.PhaseResult;
+import com.osigie.payment_gateway.domain.entity.IdempotencyKey;
+import com.osigie.payment_gateway.domain.recovery_points.AuthorizeRecoveryPoints;
+import com.osigie.payment_gateway.repository.IdempotencyKeyRepository;
+import com.osigie.payment_gateway.service.AtomicPhaseExecutor;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Service
+public class AtomicPhaseExecutorImpl implements AtomicPhaseExecutor {
+    private final IdempotencyKeyRepository idempotencyKeyRepository;
+    private final ObjectMapper objectMapper;
+
+    public AtomicPhaseExecutorImpl(IdempotencyKeyRepository idempotencyKeyRepository, ObjectMapper objectMapper) {
+        this.idempotencyKeyRepository = idempotencyKeyRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Transactional
+    @Override
+    public void execute(UUID merchantId, String idempotencyKey, Function<IdempotencyKey, PhaseResult> phase) {
+        execute(merchantId, idempotencyKey, Function.identity(), phase);
+    }
+
+
+    @Transactional
+    @Override
+    public <T> void execute(UUID merchantId, String idempotencyKey,
+                            Function<IdempotencyKey, T> loader,
+                            Function<T, PhaseResult> phase) {
+
+        IdempotencyKey key = idempotencyKeyRepository
+                .findIdempotencyForUpdate(idempotencyKey, merchantId)
+                .orElseThrow(() -> new IllegalStateException("Idempotency key not found"));
+
+
+        T resource = loader.apply(key);
+
+        PhaseResult phaseResult = phase.apply(resource);
+
+        switch (phaseResult) {
+            case PhaseResult.NoOp ignored -> {
+            }
+            case PhaseResult.RecoveryPoint recoveryPoint -> {
+                key.setRecoveryPoint(recoveryPoint.toString());
+            }
+            case PhaseResult.Response<?> response -> {
+                key.setRecoveryPoint(AuthorizeRecoveryPoints.FINISHED);
+//               TODO: think about how to handle failure and sucess result
+//                key.setResponseStatus(response.result());
+                key.setResponseBody(serialize(response.result()));
+
+            }
+        }
+        idempotencyKeyRepository.save(key);
+    }
+
+    private String serialize(Object body) {
+
+        try {
+            return objectMapper.writeValueAsString(body);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
