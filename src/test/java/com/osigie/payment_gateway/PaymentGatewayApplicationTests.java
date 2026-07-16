3162e9a114ac606929833b217b96e6288dca5578
osigie <kenosagie88@gmail.com>
2026-07-16 11:11:52 +0100

    feat: added test

--- src/test/java/com/osigie/payment_gateway/PaymentGatewayApplicationTests.java
+++ src/test/java/com/osigie/payment_gateway/PaymentGatewayApplicationTests.java
@@ -2,9 +2,11 @@ package com.osigie.payment_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class PaymentGatewayApplicationTests {
+@ActiveProfiles("test")
+class PaymentGatewayApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
